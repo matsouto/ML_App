608dd5a7ce49512f855beb5ab95cd698eead7f91
matsouto <matsouto55@gmail.com>
2022-08-20 20:32:27 -0300

    salvo no drive

--- app/src/main/java/com/souto/mltestapp/Classify.java
+++ app/src/main/java/com/souto/mltestapp/Classify.java
@@ -1,19 +1,54 @@
 package com.souto.mltestapp;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.FileProvider;
 import androidx.viewpager.widget.ViewPager;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ThumbnailUtils;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.MenuItem;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.model.Model;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
 public class Classify extends AppCompatActivity {
+
+    public TextView result, confidence;
+    public ImageView imageView;
+    public Button picture;
+    int imageSize = 224;
+
+    // Photo taken by the user is stored in this variable
+    private File photoFile;
+    private static final String FILE_NAME = "photo";
+
+    private static final int CAPTURE_IMAGE_REQUEST = 100;
 
     // Bottom nav bar
     public BottomNavigationView bottomNavigationView;
@@ -24,6 +59,24 @@ public class Classify extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN); // Hide Status
         setContentView(R.layout.activity_classify);
+
+        // If the user haven't allowed the camera permission yet, request it
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 100);
+        }
+
+        result = findViewById(R.id.result);
+        confidence = findViewById(R.id.confidence);
+        imageView = findViewById(R.id.imageView);
+        picture = findViewById(R.id.button);
+
+        picture.setOnClickListener(v -> {
+            try {
+                takePicture();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
 
         // Set up the navigation tab
         bottomNavigationView = findViewById(R.id.bottom_navigation);
@@ -49,6 +102,108 @@ public class Classify extends AppCompatActivity {
                 return true;
             }
         });
+    }
 
+    public void takePicture() throws IOException {
+        // Intent to capture the image
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        photoFile = getPhotoFile(FILE_NAME);
+
+        // Creates a Uri for the taken photo
+        Uri fileProvider = FileProvider.getUriForFile(this, "com.souto.fileprovider", photoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+
+        // Check if there is a camera
+        if(intent.resolveActivity(this.getPackageManager()) != null) {
+            startActivityForResult(intent, CAPTURE_IMAGE_REQUEST);
+        }else{
+            Toast.makeText(this,"Unable to open camera!",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private File getPhotoFile(String fileName) throws IOException {
+        // Gets taken photo and create a temporary file in the directory, returns the file
+        File storageDirectory = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        return File.createTempFile(fileName,".jpg",storageDirectory);
+    }
+
+    private void classifyImage(Bitmap image) {
+        try {
+            Model model = Model.newInstance(getApplicationContext());
+
+            // Creates inputs for reference.
+            TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 224, 224, 3}, DataType.FLOAT32);
+            ByteBuffer byteBuffer = ByteBuffer.allocateDirect(4 * imageSize * imageSize * 3);
+            byteBuffer.order(ByteOrder.nativeOrder());
+
+            int [] intValues = new int[imageSize * imageSize];
+            image.getPixels(intValues, 0, image.getWidth(), 0, 0, image.getWidth(), image.getHeight());
+            int pixel = 0;
+            for(int i = 0; i < imageSize; i++){
+                for(int j = 0; j < imageSize; j++){
+                    int val = intValues[pixel++]; //RGB
+                    byteBuffer.putFloat(((val >> 16) & 0xFF) * (1.f / 255.f));
+                    byteBuffer.putFloat(((val >> 8) & 0xFF) * (1.f / 255.f));
+                    byteBuffer.putFloat((val & 0xFF) * (1.f / 255.f));
+                }
+            }
+
+            inputFeature0.loadBuffer(byteBuffer);
+
+            // Runs model inference and gets result.
+            Model.Outputs outputs = model.process(inputFeature0);
+            TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
+
+            float[] confidences = outputFeature0.getFloatArray();
+            int maxPos = 0;
+            float maxConfidence = 0;
+            for(int i = 0; i < confidences.length; i++){
+                if(confidences[i] > maxConfidence){
+                    maxConfidence = confidences[i];
+                    maxPos = i;
+                }
+            }
+            String[] classes = {"Cat", "Dog"};
+            result.setText(classes[maxPos]);
+
+            String s = "";
+            for(int i = 0; i < classes.length; i++){
+                s += String.format("%s: %.1f%%\n", classes[i], confidences[i] * 100);
+            }
+
+            confidence.setText(s);
+
+            // Releases model resources if no longer used.
+            model.close();
+        } catch (IOException e) {
+            // TODO Handle the exception
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // To take a photo
+        if(requestCode==CAPTURE_IMAGE_REQUEST && resultCode == Activity.RESULT_OK) {
+            String filePath = photoFile.getPath();
+            Bitmap image = BitmapFactory.decodeFile(filePath);
+
+            int dimension = Math.min(image.getWidth(), image.getHeight());
+            image = ThumbnailUtils.extractThumbnail(image, dimension, dimension);
+
+//            imageView.setImageBitmap(image);
+            Glide.with(this)
+                    .load(image)
+                    .into(imageView);
+
+            // Rotating the bitmap
+            Matrix matrix = new Matrix();
+            matrix.postRotate(90);
+            image = Bitmap.createScaledBitmap(image, imageSize, imageSize, true);
+            image = Bitmap.createBitmap(image, 0, 0, image.getWidth(), image.getHeight(), matrix, true);
+
+            classifyImage(image);
+        }
     }
 }
